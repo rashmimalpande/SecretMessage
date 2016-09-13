3b746598586dfa51e75b29760d4b3852e6ed6be8
Rashmi Malpande <rashmimalpande@gmail.com>
2016-09-13 13:48:50 +0530

    Rename the files by removing the digits in the file name

--- renaming_files.py
+++ renaming_files.py
@@ -1,8 +1,19 @@
 import os
+import re
+
 
 def rename():
     # get the file names
     list = os.listdir(r"C:\Users\rashm\PycharmProjects\SecretMessage\prank")
-    print(list)
+    # print(list)
+    current = os.getcwd()
+    # print(current)
+    os.chdir(r"C:\Users\rashm\PycharmProjects\SecretMessage\prank")
+    for file_name in list:
+        new_name = re.sub(r'[0-9]', "", file_name)
+        os.rename(file_name, new_name)
+
+    os.chdir(current)
+
 
 rename()
